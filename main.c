db23b9f85e897f3d8aeb6889b569db73796cf3f5
tseng <yuchityc@gmail.com>
2024-01-27 15:00:57 +0800

    a b

--- main.c
+++ main.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 /*輸出 1 到 100 之間的奇數*/
+
 int main()
 {
-	 int a,b,c,d;
-	 for (a=1;a<=100;a++)
-	 {
-	  if(a%2==1)	       
-	  printf("%d/",a);     //輸出奇數
-	 }
-	 while(a<5)
-		 a++;
+	 int a,b;
+
+
+
+
 	 system("pause");
 	 return 0;
 }
