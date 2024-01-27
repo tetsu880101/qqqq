3cb8b519971a91c78fb4c61b0ff8e94a5ff950ce
jowlin591128 <jowlin591128@gmail.com>
2024-01-27 15:26:43 +0800

    add add function

--- main.c
+++ main.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 /*輸出 1 到 100 之間的奇數*/
-
+int add(int,int);
 int main()
 {
 	 int a,b;
-
-
+	a=5;
+	b=5;
+	printf("a+b=%d",add(a,b));
 
 
 	 system("pause");
 	 return 0;
+}
+int add(int a,int b)
+{
+	return a+b;
 }
