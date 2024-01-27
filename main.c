23a58801b6b3519864c814931829fa22625cc8b0
DDTmac <schiang9127@gmail.com>
2024-01-27 16:11:44 +0800

    add * function

--- main.c
+++ main.c
@@ -4,15 +4,15 @@
 int add(int,int);
 int m(int,int);
 int waru(int,int);
+int n(int,int);
 int main()
 {
 	 int a,b;
 	a=5;
 	b=5;
 	printf("a+b=%d",add(a,b));
-
 	printf("a-b=%d",m(a,b));
-
+	printf("a*b=%d",n(a,b));
 	printf("a/b=%d",waru(a,b));
 	system("pause");
 	 return 0;
@@ -26,6 +26,14 @@ int m(int a,int b)
 
 return a-b;
 }
+int n(int a,int b)
+{
+        return a*b;
+}
+
+
+
+ 
 int waru(int a,int b)
 {
         return a/b;
